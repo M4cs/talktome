a1fc6094908b384fe2150971e01fa5f27c2cb4e7
M4cs <mabridgland@protonmail.com>
2019-10-04 11:42:28 -0400

    Add GUI

--- talktome-client/client.py
+++ talktome-client/client.py
@@ -7,8 +7,18 @@ import sys
 import os
 import threading
 HEADER_LENGTH = 10
-class BackgroundThread(threading.Thread, GUI):
-    def run(self):
+class GUI:
+    def __init__(self, username, ip=None, port=3376, url=None):
+        self.running = False
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.username = username
+        self.url = url
+        self.ip = ip
+        self.port = port
+        self.window = None
+        self.chat_history = []
+
+    def background(self):
         while True:
             try:
                 # Now we want to loop over received messages (there might be more than one) and print them
@@ -34,7 +44,8 @@ class BackgroundThread(threading.Thread, GUI):
                     message = self.client_socket.recv(message_length).decode('utf-8')
 
                     # Print message
-                    print('\n{:<2} {:<2} {:<}'.format(user, '|', message))
+                    self.chat_history.append('{:<2} {:<2} {:<}'.format(user, '|', message))
+                    self.window.FindElement('history').Update('\n'.join(self.chat_history[-3:]))
 
             except IOError as e:
                 # This is normal on non blocking connections - when there are no incoming data error is going to be raised
@@ -52,38 +63,77 @@ class BackgroundThread(threading.Thread, GUI):
                 # Any other exception - something happened, exit
                 print('Reading error: {}'.format(str(e)))
                 sys.exit()
-                
-class GUI:
-    def __init__(self, username, client_socket, ip=None, port=3376, url=None):
-        self.running = False
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.username = username
-        self.url = url
-        self.ip = ip
-        self.port = port
+    
+    def show_window(self):
+        b = threading.Thread(name='poller', target=self.background)
+        b.daemon = True
+        b.start()
+        layout = [
+            [g.T('TalkToMe: Connected')],
+            [g.Output(size=(127, 30), key='history')],
+            [g.Multiline(size=(85, 5), enter_submits=True, key='input', do_not_clear=False)],
+            [g.B('Exit'), g.B('Send', bind_return_key=True)]
+        ]
+        self.window = g.Window('TalkToMe Client', grab_anywhere=True, keep_on_top=True, layout=layout)
+        history_offset = 0
+        while True:
+            event, values = self.window.Read()
+            print(event)
+            if 'Send' in event:
+                print('sending')
+                msg = values['input'].strip()
+                try:
+                    message = msg.encode('utf-8')
+                    message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+                    self.client_socket.send(message_header + message)
+                except:
+                    raise Exception('Unexpected Error!')
+                self.chat_history.append('{:<2} {:<1} {:<} '.format(str(self.username), '|', msg))
+                history_offset = len(self.chat_history)-1
+                self.window.FindElement('input').Update('')
+                self.window.FindElement('history').Update('\n'.join(self.chat_history[-3:]))
+            elif 'Up' in event and len(self.chat_history):
+                command = self.chat_history[history_offset]
+                history_offset -= 1 * (history_offset > 0)      # decrement is not zero
+                self.window.FindElement('input').Update(command)
+            elif 'Down' in event and len(self.chat_history):
+                history_offset += 1 * (history_offset < len(self.chat_history)-1) # increment up to end of list
+                command = self.chat_history[history_offset]
+                self.window.FindElement('input').Update(command)
+            elif 'Escape' in event:
+                self.window.FindElement('query').Update('')
+            elif 'Exit' in event:
+                self.window.Close()
+                break
+        exit()
+            
     
     def start(self):
         if self.url:
             if ':' in self.url:
                 url_split = self.url.split(':')
+                print(url_split)
                 port = url_split[1]
                 if len(port) >= 6:
                     raise Exception('Error! Port Number Must Have A Length Of 5 or Less!')
                 else:
                     self.port = port
                     try:
-                        self.ip = socket.gethostbyname(url_split[0])
+                        self.ip = socket.gethostbyname_ex(url_split[0])[2][0]
                     except:
                         raise Exception('Error! Couldnt Get The IP From That URL!')
         else:
             if self.ip == None:
                 raise Exception('Error! No IP Specified.')
-        self.client_socket.connect((self.ip, self.port))
+        print(self.ip)
+        self.client_socket.connect((self.ip, int(self.port)))
         self.client_socket.setblocking(False)
         self.running = True
         user = username.encode('utf-8')
         user_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-        client_socket.send(user_header + user)
+        self.client_socket.send(user_header + user)
+        self.show_window()
+        exit()
         
 def start_screen():
     layout = [
@@ -91,13 +141,13 @@ def start_screen():
         [g.T('Username:', justification='center')],
         [g.I('', justification='center', key='username', focus=True)],
         [g.T('Enter URL:', justification='center')],
-        [g.I('0.tcp.ngrok.io:15564', justification='center', key='url')],
+        [g.I('', justification='center', key='url')],
         [g.T('OR')],
-        [g.T('Enter IP:'), g.I('192.168.1.68', key='ip'), g.T('Enter Port:'), g.I('3376', key='port')],
+        [g.T('Enter IP:'), g.I('', key='ip'), g.T('Enter Port:'), g.I('', key='port')],
         [g.B('Cancel'), g.B('Connect')]
     ]
     
-    window = g.Window('TalkToMe', keep_on_top=True, grab_anywhere=True, no_titlebar=True, layout=layout)
+    window = g.Window('TalkToMe', keep_on_top=True, grab_anywhere=True, layout=layout)
     while True:
         event, values = window.Read()
         print(event)
@@ -109,77 +159,36 @@ def start_screen():
                 if values['username'] == '':
                     g.Window('Error', auto_close=True, auto_close_duration=2, layout=[
                         [g.T('Error! Missing Username!')]
-                    ], keep_on_top=True, grab_anywhere=True, no_titlebar=True).Read()
-            
+                    ], keep_on_top=True, grab_anywhere=True).Read()
+                else:
+                    window.Close()
+                    return values['ip'], values['port'], values['url'], values['username']
+            else:
+                if values['ip'] != '':
+                    if values['username'] == '':
+                        g.Window('Error', auto_close=True, auto_close_duration=2, layout=[
+                            [g.T('Error! Missing Username!')]
+                        ], keep_on_top=True, grab_anywhere=True).Read()
+                    if values['port'] != '':
+                        window.Close()
+                        return values['ip'], values['port'], values['url'], values['username']
+                    else:
+                        g.Window('Error', auto_close=True, auto_close_duration=2, layout=[
+                        [g.T('Error! Missing Port!')]
+                    ], keep_on_top=True, grab_anywhere=True).Read()
+                else:
+                    g.Window('Error', auto_close=True, auto_close_duration=2, layout=[
+                        [g.T('Error! Missing IP!')]
+                    ], keep_on_top=True, grab_anywhere=True).Read()
 
-# parser = ArgumentParser()
-# parser.add_argument('-ip', '--ip', help='IP Address For Chat Server')
-# parser.add_argument('-p', '--port', help='Port Number To Run Server On (Defaults To 3376)', type=int)
-
-# def clear_screen():
-#     if 'win' in sys.platform:
-#         return os.system('cls')
-#     else:
-#         return os.system('clear')
-
-# 
-    
-# args = parser.parse_args()
-# if type(args.ip) == str:
-#     hostname = socket.gethostbyname_ex(args.ip)[2][0]
-# else:
-#     hostname = args.ip
-# IP = hostname
-# PORT = args.port if args.port else 3376
-# try:
-#     columns, rows = os.get_terminal_size(0)
-# except OSError:
-#     columns, rows = os.get_terminal_size(1)
-# startup = True
-# while startup:
-#     clear_screen()
-#     print('Welcome To The TalkToMe Client!'.center(columns))
-#     print('Please Enter A Username:'.center(columns))
-#     username = input('\r\n'.center(columns - 5))
-#     while True:
-#         clear_screen()
-#         print('Confirm Your Username: {}'.format(username).center(columns))
-#         print('[Y\\N]:'.center(columns))
-#         conf = input('\r\n'.center(columns)).lower()
-#         if conf == 'y':
-#             startup = False
-#             break
-#         elif conf == 'n':
-#             break
-#         else:
-#             continue
-# clear_screen()
-# print('Joining Server Room...'.center(columns))
-# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client_socket.connect((IP, PORT))
-# client_socket.setblocking(False)
-
-# user = username.encode('utf-8')
-# user_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-# client_socket.send(user_header + user)
-            
-# b = BackgroundThread()
-# b.daemon = True
-# b.start()
-# def foreground():
-#     while True:
-#         message = input('{:<2} {:<1} {:<} '.format(str(username), '|', '>'))
-#         if message:
-#             # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
-#             if message == "exit":
-#                 clear_screen()
-#                 print('Exiting...'.center(columns))
-#                 exit()
-#             message = message.encode('utf-8')
-#             message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-#             client_socket.send(message_header + message)
-# f = threading.Thread(name='chat', target=foreground)
-# f.start()
-# exit()
+ip, port, url, username = start_screen()
+if url:
+    gui = GUI(username, url=url)
+    gui.start()
+    exit()
+else:
+    gui = GUI(username, ip=ip, port=port)
+    gui.start()
+    exit()
 
 start_screen()
